67e5a7920a8781237f81b79c2e9c6865ccb6c752
corentin-jeusset <corentin.jeusset@edu.esiee.fr>
2025-09-19 10:08:44 +0000

    programme sur les nombres premiers

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
+"""Module pour afficher les nombres premiers de 0 à 99."""
 from math import sqrt
 
-#### Fonction secondaire
-
-
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
-
+    """Retourne True si p est un nombre premier, False sinon."""
+    if p < 2:
+        return False
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """Affiche les nombres premiers de 0 à 99."""
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
 
 if __name__ == "__main__":
     main()
